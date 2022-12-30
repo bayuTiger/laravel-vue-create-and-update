dc775c09fb7d3f7243633bf86f74b954ed00ba7c
bayuTiger <68533256+bayuTiger@users.noreply.github.com>
2022-12-31 01:57:55 +0900

    add:vue.script

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,73 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">登録・更新</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('store') }}">
+                            @csrf
+                            {{-- 名前 --}}
+                            <div class="col-md-6">
+                                <input v-model="name" id="name" type="text"
+                                    class="form-control @error('name') is-invalid @enderror" name="text" required>
 
-                    {{ __('You are logged in!') }}
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+
+                            {{-- メールアドレス --}}
+                            <div class="col-md-6">
+                                <input v-model="email" id="email" type="email"
+                                    class="form-control @error('email') is-invalid @enderror" name="email" required>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+
+                            {{-- パスワード --}}
+                            <div class="col-md-6">
+                                <input v-model="password" id="password" type="password"
+                                    class="form-control @error('password') is-invalid @enderror" name="password" required>
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+    </div>
+@endsection
+
+@section('js')
+    <script>
+        const app = new Vue({
+            el: '#app',
+            data: () => {
+                return {
+                    name: '',
+                    email: '',
+                    password: '',
+                }
+            },
+            mounted: function() {
+                console.log('mounted');
+            }
+        });
+    </script>
 @endsection
